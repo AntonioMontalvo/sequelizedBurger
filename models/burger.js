74e594f642b850bbaefe65a60cb4abdd4d80aec4
Antonio Montalvo <montalvocode@yahoo.com>
2016-10-11 11:38:44 -0700

    add .gitignore, configure config.json, create burger model

--- models/burger.js
+++ models/burger.js
@@ -1,27 +1,31 @@
-/*
-Here is where you setup a model for how to interface with the database.
-*/
-//
-var orm = require('../config/orm.js');
+module.exports = function(sequelize, DataTypes){
+	var Burger = sequelize.define('Burger', {
+		// we save a primary id as an auto-incrementing int.
+		id: {
+			type: DataTypes.INTEGER,
+			autoIncrement: true,
+			primaryKey: true
+		},
+		burger_name: {
+			type: DataTypes.STRING
+		},
+		devoured: {
+			type: Sequelize.BOOLEAN,
+			defaultValue: false
+		}
+	}, {
+		classMethods: {
+			// OUR hasOne() RELATION
+			// associate gets called within index.js,
+			// and mergers this model with models.Store
+			// The foreign key will be placed on the Store table
 
-//this object contains methods to call the methods on orm and to handle the response for each callback to the methods on orm.
-var burger = {
-	selectAll: function (cb) {
-		orm.selectAll('burgers', function(res){
-			cb(res);
-		});
-	},
-	insertOne: function (cols, vals, cb) {
-		//the burger.insertOne method passes 2 arguments to orm.insertOne. The 'burgers' argument refers to the burgers table. cols refers to the burger_name column in the burgers table. The actual value comes from the request body name [req.body.burger_name] given through router.post. index.handlebars gives this information to router.post when the user clicks on the Devour it! button in the  form. The form has the action='index/create' and method 'POST' for this. And the callback function.
-		orm.insertOne('burgers', cols, vals, function(res){
-			cb(res);
-		});
-	},
-	updateOne: function (objColVals, condition, cb) {
-		orm.updateOne('burgers', objColVals, condition, function(res){
-			cb(res);
-		});
-	}
-};
+      associate: function(models) {
+      	//
+      }
+    }
+  })
 
-module.exports = burger;
+	// Finally, we return the Manager model to the Module
+	return Manager;
+}
